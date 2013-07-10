1082b768d46d06fc4ec94c0843b1727e6b3c0e0e
Michael Mulley <michael@michaelmulley.com>
2013-07-10 17:20:14 -0400

    Iterating on build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
 		concat: {
 			main: {
 				files: {
-					'<%=dest%>/app/open511-viewer.js': [
+					'<%=dest%>/js/open511-viewer.js': [
 						'app/js/no-i18n.js',
 						'app/js/main.js',
 						'app/js/layout.js',
@@ -22,20 +22,20 @@ module.exports = function(grunt) {
 						'app/js/filterwidget.js',
 						'app/js/widgets.js',
 						'app/js/utils.js',
-						'<%=dest%>/app/templates.js'
+						'<%=dest%>/js/templates.js'
 					],
-					'<%=dest%>/app/map-adapter-leaflet.js': ['app/js/map-leaflet.js'],
-					'<%=dest%>/app/map-adapter-google.js': ['app/js/map-google.js', 'app/js/geojson-to-google.js'],
-					'<%=dest%>/app/open511-editor.js': [
+					'<%=dest%>/js/map-adapter-leaflet.js': ['app/js/map-leaflet.js'],
+					'<%=dest%>/js/map-adapter-google.js': ['app/js/map-google.js', 'app/js/geojson-to-google.js'],
+					'<%=dest%>/js/open511-editor.js': [
 						'app/js/editor/editor.js',
 						'app/js/editor/widgets/map.js',
 						'app/js/editor/widgets/roads.js',
 						'app/js/editor/widgets/attachments.js',
 						'app/js/plugins/publish-events.js',
-						'<%=dest%>/app/templates-editor.js'
+						'<%=dest%>/js/templates-editor.js'
 					],
 
-					'<%=dest%>/libs/main.js': [
+					'<%=dest%>/js/libs-main.js': [
 						'app/vendor/jquery.js',
 						'app/vendor/lodash.js',
 						'app/vendor/backbone.js',
@@ -46,17 +46,17 @@ module.exports = function(grunt) {
 						'app/vendor/bootstrap/js/bootstrap-modal.js',
 						'app/vendor/datepicker.js'
 					],
-					'<%=dest%>/libs/editor.js': [
+					'<%=dest%>/js/libs-editor.js': [
 						'app/vendor/jquery/jquery.ui.widget.js',
 						'app/vendor/fileupload/iframe-transport.js',
 						'app/vendor/fileupload/jquery.fileupload.js'
 					],
-					'<%=dest%>/libs/all-googlemaps.js': [
-						'<%=dest%>/libs/main.js',
-						'<%=dest%>/libs/editor.js'
+					'<%=dest%>/js/libs-all-googlemaps.js': [
+						'<%=dest%>/js/libs-main.js',
+						'<%=dest%>/js/libs-editor.js'
 					],
-					'<%=dest%>/libs/all-leafletmaps.js': [
-						'<%=dest%>/libs/all-googlemaps.js',
+					'<%=dest%>/js/libs-all-leaflet.js': [
+						'<%=dest%>/js/libs-all-googlemaps.js',
 						'app/vendor/leaflet/leaflet.js',
 						'app/js/leaflet.draw.js'
 					],
@@ -66,32 +66,43 @@ module.exports = function(grunt) {
 						'app/i18n/fr.js'
 					],
 
-					'<%=dest%>/app/main.css': ['app/css/index.css'],
-					'<%=dest%>/app/editor.css': ['app/css/editor.css'],
-					'<%=dest%>/libs/libs.css': [
+					'<%=dest%>/css/main.css': ['app/css/index.css'],
+					'<%=dest%>/css/editor.css': ['app/css/editor.css'],
+					'<%=dest%>/css/libs.css': [
 						'app/vendor/bootstrap/css/bootstrap.css',
 						'app/vendor/datepicker.css'
 					],
-					'<%=dest%>/libs/leaflet.css': [
+					'<%=dest%>/css/leaflet.css': [
 						'app/vendor/leaflet/leaflet.css',
 						'app/vendor/leaflet/leaflet.draw.css'
 					],
 
-					'<%=dest%>/open511-complete-leaflet.js': [
-						'<%=dest%>/libs/all-leafletmaps.js',
-						'<%=dest%>/app/open511-viewer.js',
-						'<%=dest%>/app/open511-editor.js',
-						'<%=dest%>/app/map-adapter-leaflet.js'
+					'<%=dest%>/js/open511-complete-leaflet.js': [
+						'<%=dest%>/js/libs-all-leaflet.js',
+						'<%=dest%>/js/open511-viewer.js',
+						'<%=dest%>/js/open511-editor.js',
+						'<%=dest%>/js/map-adapter-leaflet.js'
 					],
-					'<%=dest%>/open511-complete-googlemaps.js': [
-						'<%=dest%>/libs/all-googlemaps.js',
-						'<%=dest%>/app/open511-viewer.js',
-						'<%=dest%>/app/open511-editor.js',
-						'<%=dest%>/app/map-adapter-google.js'
+					'<%=dest%>/js/open511-complete-googlemaps.js': [
+						'<%=dest%>/js/libs-all-googlemaps.js',
+						'<%=dest%>/js/open511-viewer.js',
+						'<%=dest%>/js/open511-editor.js',
+						'<%=dest%>/js/map-adapter-google.js'
 					],
 
 					'<%=dest%>/example.html': ['app/example.html']
 				}
+			}
+		},
+
+		copy: {
+			main: {
+				files: [{
+					expand: true,
+					flatten: true,
+					src: ['app/img/*', 'app/vendor/bootstrap/img/*'],
+					dest: '<%=dest%>/img/'
+				}]
 			}
 		},
 
@@ -103,19 +114,19 @@ module.exports = function(grunt) {
 			},
 			viewer: {
 				src: ['app/jst/*.html'],
-				dest: '<%=dest%>/app/templates.js'
+				dest: '<%=dest%>/js/templates.js'
 			},
 			editor: {
 				src: ['app/jst/editor/*.html'],
-				dest: '<%=dest%>/app/templates-editor.js'
+				dest: '<%=dest%>/js/templates-editor.js'
 			}
 		},
 
 		uglify: {
 			main: {
 				files: {
-					'<%=dest%>/open511-complete-leaflet.min.js': ['<%=dest%>/open511-complete-leaflet.js'],
-					'<%=dest%>/open511-complete-googlemaps.min.js': ['<%=dest%>/open511-complete-googlemaps.js'],
+					'<%=dest%>/js/open511-complete-leaflet.min.js': ['<%=dest%>/js/open511-complete-leaflet.js'],
+					'<%=dest%>/js/open511-complete-googlemaps.min.js': ['<%=dest%>/js/open511-complete-googlemaps.js'],
 
 					'<%=dest%>/locale/fr.js': ['<%=dest%>/locale/fr.js']
 				}
@@ -125,14 +136,14 @@ module.exports = function(grunt) {
 		cssmin: {
 			main: {
 				files: {
-					'<%=dest%>/open511-complete-googlemaps.css': [
-						'<%=dest%>/app/main.css',
-						'<%=dest%>/app/editor.css',
-						'<%=dest%>/libs/libs.css'
+					'<%=dest%>/css/open511-complete-googlemaps.css': [
+						'<%=dest%>/css/main.css',
+						'<%=dest%>/css/editor.css',
+						'<%=dest%>/css/libs.css'
 					],
-					'<%=dest%>/open511-complete-leaflet.css': [
-						'<%=dest%>/open511-complete-googlemaps.css',
-						'<%=dest%>/libs/leaflet.css'
+					'<%=dest%>/css/open511-complete-leaflet.css': [
+						'<%=dest%>/css/open511-complete-googlemaps.css',
+						'<%=dest%>/css/leaflet.css'
 					]
 				}
 			}
@@ -152,12 +163,17 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
 	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-	grunt.registerTask('assemble', ['clean', 'jst', 'concat', 'cssmin']);
+	grunt.registerTask('assemble', ['clean', 'jst', 'concat', 'copy', 'cssmin']);
 	grunt.registerTask('default', ['assemble', 'uglify']);
 	grunt.registerTask('python-build', function() {
 		grunt.config('dest', 'open511_ui/static');
 		grunt.task.run('default');
+	});
+	grunt.registerTask('python-watch', function() {
+		grunt.config('dest', 'open511_ui/static');
+		grunt.task.run('watch');
 	});
 
 };
