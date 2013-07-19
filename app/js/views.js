e424a0e56a2eb1cecef1f7ee4dda53baa01636f9
Michael Mulley <michael@michaelmulley.com>
2013-07-19 17:50:44 -0400

    Allow left-pane blurb to be overridden in multiple languages

--- app/js/views.js
+++ app/js/views.js
@@ -13,7 +13,13 @@ O5.views.BlurbView = O5.views.BaseView.extend({
 		O5.views.BaseView.prototype.initialize.call(this);
 
 		var html = this.app.settings.blurbHTML;
-		if (!html) {
+		if (html) {
+			if (_.isObject(html)) {
+				if (!html['default'] && html.en) html['default'] = html.en;
+				html = html[O5.language] ? html[O5.language] : html['default'];
+			}
+		}
+		else {
 			html = '<h2>' + O5._t('Welcome to Open511') + '</h2>' +
 				'<p>' + O5._t('Open511 is the open standard for sharing road and construction data. Read more at <a href="http://www.open511.org/">open511.org</a>.') + '</p>';
 		}
